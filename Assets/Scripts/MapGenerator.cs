93c9feb8c1bfde24c607a822bf1a0b056b23ddbc
NickPfeiffer <60349846+NickPfeiffer@users.noreply.github.com>
2021-05-08 14:00:04 +0200

    improved mapGenerator (Episode 3)

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,48 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+
+    // values greater than 1 have no significant impact
+    [Range(0, 1)] public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
-    
+
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance,
+            lacunarity, offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    // clamp values
+    private void OnValidate()
+    {
+        if (mapWidth < 0)
+        {
+            mapWidth = 1;
+        }
+
+        if (mapHeight < 0)
+        {
+            mapHeight = 0;
+        }
+
+        // negative values are mirrored
+        if (lacunarity < 0)
+        {
+            lacunarity = 1;
+        }
+
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
     }
 }
